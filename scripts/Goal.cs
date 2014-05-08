5288dd1b6e0089089791a665f1ff1f518cad25b2
Sander <sandermoolin@gmail.com>
2014-05-07 23:38:23 -0400

    Started goal skeleton

---
+++ scripts/Goal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+public class Goal
+{
+	public Goal (string gN, string d, GoalType gT, bool c = false)
+	{
+		goalName = gN;
+		description = d;
+		type = gT;
+		complete = c;
+	}
+	
+	public enum GoalType
+	{
+		Habit,
+		Daily,
+		OneTime
+	};
+	
+	// snappy name
+	public string goalName;
+	// brief description
+	public string description;
+	// type of goal
+	public GoalType type;
+	// is the goal done for the day?
+	public bool complete;
+}
--- scripts/User.cs
+++ scripts/User.cs
@@ -1,15 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
 
 public class User : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	public string userName;
+	public List<Goal> goals = new List<Goal>();
+
+	public int level;
+
+	public float maintenancePoints;
+
+
+
 }
